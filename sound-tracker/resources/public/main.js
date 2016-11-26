71aa3293cd6e561508e9b07e28a0e3672c1cb0be
jableader <jacobdunk@gmail.com>
2016-11-26 15:37:49 +1100

    Sound processing returning number between zero and one

--- sound-tracker/resources/public/main.js
+++ sound-tracker/resources/public/main.js
@@ -1,1 +1,29 @@
 /* I am javascript */
+(function() {
+  var success = function(stream) {
+    var context = new AudioContext();
+    var source = context.createMediaStreamSource(stream);
+    var processor = context.createScriptProcessor(2048, 1, 1);
+
+    processor.onaudioprocess = function(evt) {
+      var inputBuffer = evt.inputBuffer.getChannelData(0);
+      var sumSq = 0;
+
+      var len = inputBuffer.length;
+      for (var i = 0; i < len; i++) {
+        sumSq += Math.abs(inputBuffer[i]);
+      }
+
+      transmit(Math.sqrt(sumSq / len))
+    };
+
+    source.connect(processor);
+    processor.connect(context.destination);
+  };
+
+  var error= function(e) {
+    console.log("Nah mate", e);
+  }
+
+  navigator.getUserMedia({audio: true}, success, error);
+})();
