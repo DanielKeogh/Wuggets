238abb946a64cd9c5d285f7ba8ba6e1b351370a4
danielkeogh <keogh.daniel@gmail.com>
2016-11-26 16:40:51 +1100

    more stuff

--- sound-tracker/src/sound_tracker/pages.clj
+++ sound-tracker/src/sound_tracker/pages.clj
@@ -31,13 +31,19 @@
     [:div.container body]
     (hpage/include-js "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js")
     (hpage/include-js "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js")
-    (hpage/include-js "main.js")]))
+    (hpage/include-js "viewheat.js")]))
 
 (defn get-heat-canvas [img-source]
-  [:div#heatview
-   [:img {:class "img" :src img-source :alt "A map."}]
-   [:canvas#heatcanvas {:width 640 :height 480}]
-   [:button]])
+  [:div#heatviewcontainer
+   [:div#heatview
+    [:img {:class "img" :src img-source :alt "A map."}]
+    [:canvas#heatcanvas {:width 640 :height 480}]]
+   [:form#view-range {:onsubmit "return formposted()"}
+    [:input {:type "datetime" :value "2011-01-13"}]
+    [:input {:type "datetime" :value "2011-01-13"}]
+    [:input {:type "submit" :value "View Period"} ]
+    ]
+   ])
 
 (defn home []
   (core-page [[:title "Wuggets Core"]]
