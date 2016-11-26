610959fb32b7264f16b8d983ebe435965ea2712b
jableader <jacobdunk@gmail.com>
2016-11-26 16:23:12 +1100

    Reduced to 10S/s, added median filter

--- sound-tracker/resources/public/main.js
+++ sound-tracker/resources/public/main.js
@@ -1,9 +1,21 @@
 /* I am javascript */
-(function() {
+window.audioprocess = (function() {
+  var exp = {};
+
   var success = function(stream) {
     var context = new AudioContext();
     var source = context.createMediaStreamSource(stream);
     var processor = context.createScriptProcessor(2048, 1, 1);
+
+    var rmsBuffer = new Array(100, 0.5);
+    var rmsBufferIndex = 0;
+
+    window.setInterval(function() {
+      var cpy = rmsBuffer.slice();
+      cpy.sort()
+      if (exp.transmit)
+        exp.transmit(cpy[cpy.length / 2])
+    }, 100);
 
     processor.onaudioprocess = function(evt) {
       var inputBuffer = evt.inputBuffer.getChannelData(0);
@@ -14,18 +26,24 @@
         sumSq += Math.abs(inputBuffer[i]);
       }
 
-      transmit(Math.sqrt(sumSq / len))
+      rmsBufferIndex = (rmsBufferIndex + 1) % rmsBuffer.length;
+      rmsBuffer[rmsBufferIndex] = Math.sqrt(sumSq / len);
     };
 
     source.connect(processor);
     processor.connect(context.destination);
+
+    if (exp.success)
+      exp.success();
   };
 
   var error= function(e) {
     console.log("Nah mate", e);
+    if (audioproc.onconnecterror)
+      audioproc.onconnecterror(e);
   }
 
   navigator.getUserMedia({audio: true}, success, error);
 
-  window.audioproc = {}
+  return exp
 })();
