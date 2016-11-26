f1ecfedcd2cfcf31a1f610cc60106ade0e46cdc5
danielkeogh <keogh.daniel@gmail.com>
2016-11-26 13:06:46 +1100

    add static resources

--- sound-tracker/src/sound_tracker/handler.clj
+++ sound-tracker/src/sound_tracker/handler.clj
@@ -6,8 +6,9 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (GET "/" [] (pages/home))
+  (route/resources "/")
+  (route/not-found "404 Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
--- sound-tracker/src/sound_tracker/pages.clj
+++ sound-tracker/src/sound_tracker/pages.clj
@@ -5,4 +5,14 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defn core-page [head body]
-  ())
+  (hc/html 
+   (into [:head] head)
+   (into [:body] body)))
+
+(defn get-view []
+  [:div.view
+   [:canvas.map]])
+
+(defn home []
+  (core-page [[:title "Wuggets Core"]]
+             [[:h1 "Page Viewer"]]))
